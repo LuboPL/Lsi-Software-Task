f9ab38b8787f6042a033c3525d02e7a223a91df8
LuboPL <lubaskamil@gmail.com>
2025-12-20 14:33:20 +0100

    wait-db script fix

--- scripts/wait-db.sh
+++ scripts/wait-db.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
-until docker compose exec -T mysql sh -c 'mysqladmin ping -u"$$MYSQL_USER" -p"$$MYSQL_PASSWORD" -h 127.0.0.1 --silent >/dev/null 2>&1'; do \
+until docker compose exec -T mysql sh -c 'mysqladmin ping -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h 127.0.0.1 --silent >/dev/null 2>&1'; do \
   sleep 1; \
 done
 
-until docker compose exec -T mysql sh -c 'mysql -u"$$MYSQL_USER" -p"$$MYSQL_PASSWORD" -e "USE $$MYSQL_DATABASE" >/dev/null 2>&1'; do \
+until docker compose exec -T mysql sh -c 'mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "USE $MYSQL_DATABASE" >/dev/null 2>&1'; do \
   sleep 1; \
 done
